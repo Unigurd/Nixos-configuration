32eea881b234082fff3d1052a61e6f7d9cb4b607
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-02-04 23:29:27 +0100

    Change hostname from 'nixos' to 'gurd-personal'

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@
   # [      fsType = "vfat"
   # [    };
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "gurd-personal"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
