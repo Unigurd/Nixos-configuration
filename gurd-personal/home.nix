2c09b2cf31fbddc529b4dad1dda28471e51ea752
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-02-17 13:35:14 +0100

    Add remaining dotfiles to gurd-personal home

--- gurd-personal/home.nix
+++ gurd-personal/home.nix
@@ -3,5 +3,8 @@
 {
   home.file.".gitconfig".source = ../dotfiles/.gitconfig;
   home.file.".Xresources".source = ../dotfiles/.Xresources;
+  home.file.".config/i3/config".source = ../dotfiles/.config/i3/config;
+  home.file.".bashrc".source = ../dotfiles/.bashrc;
+
   home.stateVersion = "23.11";
 }
