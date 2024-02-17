e18b82b58be3e28a0676f2d054ff50f23a26d270
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-02-17 13:42:14 +0100

    Add dotfiles to gurd-server home

--- gurd-server/home.nix
+++ gurd-server/home.nix
@@ -8,11 +8,11 @@
 
   home.file.".gitconfig".source = ../dotfiles/.gitconfig;
   home.file.".Xresources".source = ../dotfiles/.Xresources;
+  home.file.".bashrc".source = ../dotfiles/.bashrc;
+  home.file.".config/i3/config".source = ../dotfiles/.config/i3/config;
 
   # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
+  # compatible with.
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
