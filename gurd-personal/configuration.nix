8a32da94596068e19573257bdfbf083c512bd47c
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-03-21 22:21:18 +0100

    Organize gurd-personal config

--- gurd-personal/configuration.nix
+++ gurd-personal/configuration.nix
@@ -7,33 +7,54 @@
 {
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  imports = [./hardware-configuration.nix];
 
-  imports =
-    [./hardware-configuration.nix];
+  # User gurd
+  users.users.gurd = {
+    initialPassword = "sabre";
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs;
+      [
+        firefox
+        evince
+        texlive.combined.scheme-full
+        inputs.bash-utils.packages."${pkgs.system}".label
+        inputs.bash-utils.packages."${pkgs.system}".note
+      ];
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    vim wget emacs tmux guile_3_0 git keepassxc pkgs.man-pages pkgs.man-pages-posix];
+
+  # Developer documentation (How does this compare to pkgs.man-pages(-posix)?
+  documentation.dev.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.editor = false;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # [  fileSystems."/mnt/boot" =
-  # [    { device = "/dev/disk/by-label/BOOT";
-  # [      fsType = "vfat"
-  # [    };
-
+  # Networking
   networking.hostName = "gurd-personal"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  time.timeZone = "CET"; # "Europe/Amsterdam";
+  # Docker
+  virtualisation.docker.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Time zone
+  time.timeZone = "Europe/Copenhagen";
 
-  # Select internationalisation properties.
+  # Internationalisation
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
@@ -41,24 +62,17 @@
     useXkbConfig = true; # use xkbOptions in tty.
   };
 
-  # Enable the X11 windowing system.
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-  # environment.pathsToLink = ["/libexec"];
-
+  # Enable the X11 windowing system
   services.xserver = {
     enable = true;
-
     xkb.options = "grp:win_space_toggle";
     xkb.layout = "us,dk";
-
-    desktopManager = {
-    xterm.enable = false;
-    };
-
-    displayManager = {
-      defaultSession = "none+i3";
-    };
-
+    libinput.enable = true; # Enable touchpad support (enabled default in most desktop managers)
+    desktopManager.xterm.enable = false;
+    displayManager.defaultSession = "none+i3";
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
@@ -72,7 +86,6 @@
     Section "Monitor"
       Identifier "eDP-1
     EndSection
-
     Section "Monitor"
       Identifier "HDMI-2"
       Option "Above" "eDP-1"
@@ -80,95 +93,7 @@
 '';
   };
 
-  nixpkgs.config.allowUnfree = true;
-
-  virtualisation.docker.enable = true;
-
-  # virtualisation.virtualbox.host.enable = true;
-  # virtualisation.virtualbox.host.enableHardening = false;
-  # virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.extraGroups.vboxusers.members = [ "gurd" ];
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable bluetooth
-  hardware.bluetooth.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.gurd = {
-    initialPassword = "sabre";
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs;
-      # let bash-utils = (builtins.getFlake # "github:Unigurd/bash-utils").packages.x86_64-linux;
-      #                                     "/home/gurd/bash/bash-utils").packages.x86_64-linux;
-      [
-        firefox
-        thunderbird
-        evince
-        texlive.combined.scheme-full
-        inputs.bash-utils.packages."${pkgs.system}".label
-        inputs.bash-utils.packages."${pkgs.system}".note
-      ];
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    emacs
-    tmux
-    guile_3_0
-    git
-    keepassxc
-    pkgs.man-pages
-    pkgs.man-pages-posix
-  ];
-
-
-  documentation.dev.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # DO NOT CHANGE
   system.stateVersion = "22.05"; # Did you read the comment?
 
 }
