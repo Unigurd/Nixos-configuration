8d4bd1a8c6d1c629ea43c542abf91f29a19e5a78
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-10-17 20:08:22 +0200

    Install emacs packages

--- gurd-personal/configuration.nix
+++ gurd-personal/configuration.nix
@@ -14,7 +14,34 @@
 
   # System packages
   environment.systemPackages = with pkgs; [
-    vim emacs
+    ((emacsPackagesFor emacs).emacsWithPackages (
+      epkgs: with epkgs; [
+        # ace
+        ace-jump-mode ace-window
+        # casual
+        # casual-symbol-overlay missing for some
+        # reason. Install from melpa instead.
+        casual-dired casual-info casual-ibuffer
+        casual-isearch casual-re-builder
+        # completion and stuff
+        vertico orderless corfu embark marginalia
+        # evil
+        evil evil-paredit
+        # Lisp
+        geiser geiser-guile slime
+        # Git
+        magit git-timemachine
+        # highlighting
+        highlight-indent-guides rainbow-delimiters smart-mode-line
+        symbol-overlay
+        # undo
+        vundo undo-fu-session
+        # various
+        flycheck gcmh auctex arduino-mode ediprolog haskell-mode
+        nix-mode font-lock-studio treesit-auto paredit
+      ]
+    ))
+    vim
     dig wget
     tmux
     git
