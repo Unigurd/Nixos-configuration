8dd918548d3d4aaa34f815fa21d4f745c14123ff
Sigurd Dam Sonniks <sson@baselifescience.com>
2024-10-23 21:51:19 +0200

    Add emacs.nix

---
+++ lib/emacs.nix
@@ -0,0 +1,31 @@
+pkgs:
+(pkgs.emacsPackagesFor pkgs.emacs30).emacsWithPackages (
+  epkgs: with epkgs; [
+    # ace
+    ace-jump-mode ace-window
+    # casual
+    # casual-symbol-overlay missing for some
+    # reason. Install from melpa instead.
+    casual-dired casual-info casual-ibuffer
+    casual-isearch casual-re-builder
+    # completion and stuff
+    vertico orderless corfu embark marginalia
+    # evil
+    # These are not used because evil fails to native-compile
+    # evil evil-paredit
+    # Lisp
+    geiser geiser-guile slime
+    # Git
+    magit git-timemachine
+    # highlighting
+    highlight-indent-guides rainbow-delimiters smart-mode-line
+    symbol-overlay
+    # undo
+    vundo undo-fu-session
+    # various
+    flycheck gcmh auctex arduino-mode ediprolog haskell-mode
+    nix-mode font-lock-studio paredit eglot hydra fold-dwim
+    # tree-sitter
+    treesit-grammars.with-all-grammars treesit-auto
+  ]
+)
--- sson/home.nix
+++ sson/home.nix
@@ -20,37 +20,7 @@
   };
 
   home.packages = with pkgs; [
-    ((emacsPackagesFor emacs30).emacsWithPackages (
-      epkgs: with epkgs; [
-        # ace
-        ace-jump-mode ace-window
-        # casual
-        # casual-symbol-overlay missing for some
-        # reason. Install from melpa instead.
-        casual-dired casual-info casual-ibuffer
-        casual-isearch casual-re-builder
-        # completion and stuff
-        vertico orderless corfu embark marginalia
-        # evil
-        # These are not used because evil fails to native-compile
-        # evil evil-paredit
-        # Lisp
-        geiser geiser-guile slime
-        # Git
-        magit git-timemachine
-        # highlighting
-        highlight-indent-guides rainbow-delimiters smart-mode-line
-        symbol-overlay
-        # undo
-        vundo undo-fu-session
-        # various
-        flycheck gcmh auctex arduino-mode ediprolog haskell-mode
-        nix-mode font-lock-studio paredit eglot hydra fold-dwim
-        # tree-sitter
-        treesit-grammars.with-all-grammars treesit-auto
-      ]
-    ))
-
+    (import ../lib/emacs.nix pkgs)
     python312Packages.python-lsp-server
     nil
     nodejs  # Needed for the emacs copilot package
